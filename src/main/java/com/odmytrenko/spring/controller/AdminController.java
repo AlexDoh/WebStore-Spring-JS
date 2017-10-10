52264aa45172a3e0abf291ce9a3b792662f557bd
Alex <odmitrenko1@gmail.com>
2017-10-10 19:26:54 +0300

    Changed redirection after successful operation to previous page.

--- src/main/java/com/odmytrenko/spring/controller/AdminController.java
+++ src/main/java/com/odmytrenko/spring/controller/AdminController.java
@@ -49,6 +49,13 @@ public class AdminController {
         return mv;
     }
 
+    @RequestMapping(value = "/adminconsole/deleteuser/")
+    public ModelAndView deleteUserReturn() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("userManagement");
+        return mv;
+    }
+
     @RequestMapping(value = "/adminconsole/deleteuser")
     public ModelAndView deleteUser(@RequestParam String userName) {
         ModelAndView mv = new ModelAndView();
@@ -66,6 +73,13 @@ public class AdminController {
         mv.addObject("name", userName);
         mv.addObject("type", "User");
         mv.setViewName("performedAction");
+        return mv;
+    }
+
+    @RequestMapping(value = "/adminconsole/manageuser/")
+    public ModelAndView updateUserReturn() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("userManagement");
         return mv;
     }
 
@@ -93,6 +107,17 @@ public class AdminController {
         return mv;
     }
 
+    @RequestMapping(value = "/adminconsole/managecategory")
+    public ModelAndView manageCategory() {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("category", new Category());
+        mv.addObject("product", new Product());
+        mv.addObject("categories", categoryService.getAll());
+        mv.addObject("products", productService.getAll());
+        mv.setViewName("adminConsole");
+        return mv;
+    }
+
     @RequestMapping(value = "/adminconsole/managecategory", method = RequestMethod.POST)
     public ModelAndView manageCategory(@ModelAttribute("category") Category category, @RequestParam String action) {
         switch (action) {
@@ -107,6 +132,17 @@ public class AdminController {
         mv.addObject("name", category.getName());
         mv.addObject("type", "Category");
         mv.setViewName("performedAction");
+        return mv;
+    }
+
+    @RequestMapping(value = "/adminconsole/manageproduct")
+    public ModelAndView manageProduct() {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("category", new Category());
+        mv.addObject("product", new Product());
+        mv.addObject("categories", categoryService.getAll());
+        mv.addObject("products", productService.getAll());
+        mv.setViewName("adminConsole");
         return mv;
     }
 
--- src/main/webapp/WEB-INF/views/performedAction.jsp
+++ src/main/webapp/WEB-INF/views/performedAction.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -15,7 +16,7 @@
         }
     %>
     ${requestScope.name} processed</h1>
-<a href="${pageContext.request.contextPath}/"
-   style="position: relative;bottom: -20px;">Back to main page</a><br>
+<a href="${requestScope.get("javax.servlet.forward.request_uri")}/"
+   style="position: relative;bottom: -20px;">Back to previous page</a><br>
 </body>
 </html>
